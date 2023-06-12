9063d1c80284f1232cacd15a50f65d7e9fad6edd
Yahya Sakerwala <yahyasakarwala1@gmail.com>
2023-06-13 02:39:41 +0530

    2. Query to Find all suppliers who provide products in the ‘Seafood’ category

--- Query_2.sql
+++
@@ -1,7 +0,0 @@
-/* Query to Find all suppliers who provide products in the ‘Seafood’ category */
-
-SELECT distinct suppliers.supplier_id, suppliers.company_name, suppliers.contact_name
-FROM suppliers
-INNER JOIN products ON suppliers.supplier_id = products.supplier_id
-INNER JOIN categories ON products.category_id = categories.category_id
-WHERE categories.category_name = 'Seafood';
